ab361d388ba8e990c2811e118e602f50d7b221da
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-04-28 12:47:29 +0900

    [add]  コントローラにアクションを設定

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,23 @@
 class TodosController < ApplicationController
+
+  def index
+    @todo = Todo.new
+    @todos = Todo.create("created_at DESC")
+  end
+
+  def create
+    @todo = Todo.new(toso_params)
+    if @todo.save
+      redirect_to :root
+    else
+      render :index, alert: "Todoを入力してください"
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:content)
+  end
+
 end
